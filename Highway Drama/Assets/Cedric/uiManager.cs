e7ea31b018f9cbdc692c7c8ba87d5929545de02f
Cedric Lim <cedricll@uci.edu>
2019-06-01 15:41:09 -0700

    kkkk

---
+++ Highway Drama/Assets/Cedric/uiManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class uiManager : MonoBehaviour
+{
+    public Text scoreText;
+    int score;
+    // Start is called before the first frame update
+    void Start()
+    {
+        score = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        scoreText.text = "Score: " + score;
+    }
+
+    void scoreUpdate()
+    {
+        score += 1;
+    }
+
+    public void Pause()
+    {
+        if (Time.timeScale == 1)
+        {
+            Time.timeScale = 0;
+        }
+        else if (Time.timeScale == 0)
+        {
+            Time.timeScale = 1;
+        }
+    }
+}
